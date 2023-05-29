dd846313b2fa364c56386d22080d9f3ddfef12c7
MichaelLin12 <michael.lin2032@gmail.com>
2023-05-29 15:07:27 -0500

    Learning

---
+++ HigherOrLower/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HigherOrLower{
+    public class Program{
+        public static void Main(string[] args){
+            Random random = new Random();
+            int randomNumber = random.Next(1,100);
+            int guess = 10;
+            while(guess > 0){
+                Console.Write("Geuss a number between 1 and 100: ");
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+                if(userGuess == randomNumber){
+                    Console.WriteLine("You guessed correctly!");
+                    break;
+                }else if(userGuess > randomNumber){
+                    Console.WriteLine("Your guess was too high");
+                }else{
+                    Console.WriteLine("Your guess was too low");
+                }
+                guess--;
+            }
+        }
+    }
+}
